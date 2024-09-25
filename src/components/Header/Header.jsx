9b7855b60a8fca18b42453d83607266061fe1185
lwambon <lwambonyaga@gmail.com>
2024-09-25 13:59:17 +0300

    creating a react website for muranga university tech club

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import './Header.css'
+
+function HeaderNavLinks (){
+    return(
+        <section className="header-section">
+            <div className="header-title">
+                <h2 className="header-logo">Muranga university tech club</h2>
+            </div>
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-items"><a href="#" className="list-item">Home</a></li>
+                    <li className="nav-list-items"><a href="#about" className="list-item">about</a></li>
+                    <li className="nav-list-items"><a href="#leadership" className="list-item">leadership</a></li>
+                    <li className="nav-list-items"><a href="#tracks" className="list-item">tracks</a></li>
+                    <li className="nav-list-items"><a href="#events" className="list-item">events</a></li>
+                </ul>
+            </nav>
+            
+        </section>
+    )
+}
+function Header() {
+  return (
+    <div>
+        <HeaderNavLinks/>
+    </div>
+  )
+}
+
+export default Header
