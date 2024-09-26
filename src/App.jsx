b27b5a344539f9122e3852e6a8e9c3b9bfb98e47
lwambon <lwambonyaga@gmail.com>
2024-09-26 14:37:53 +0300

    heros section of muranga university tech club website

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header/Header'
+import Heros from './components/Heros/Heros'
 import About from './components/About/About'
 import './App.css'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Heros/>
       <About/>
    
     </>
---
+++ src/components/Heros/Heros.jsx
@@ -0,0 +1,16 @@
+import './Heros.css'
+
+function Heros() {
+  return (
+    <div className="hero-section">
+        <div className="heros-content-areas">
+            <h2 className="heros-text-area">welcome to muranga tech club</h2>
+            <p className="heros-description-area">Code, Create, and Transform the World of Technology !</p>
+            <button className="btn-explore">explore now</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Heros
